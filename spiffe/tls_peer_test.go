3e83b5aa2e476896d7416f9adb7e5c6553b2f5a0
Vitalii Levitskii <vitalii@uber.com>
2024-09-12 11:10:46 +0200

    Changed grpc testing package import path

--- spiffe/tls_peer_test.go
+++ spiffe/tls_peer_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spiffe/go-spiffe/spiffetest"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/grpc_testing"
+	"google.golang.org/grpc/interop/grpc_testing"
 )
 
 func TestTLSPeer(t *testing.T) {
